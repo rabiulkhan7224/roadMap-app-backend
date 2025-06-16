b58286ffcaf5e656c0f09b49a1a887be5af047ea
rabiulkhan7224 <mdrabiulkhanbabo@gmail.com>
2025-06-16 23:48:22 +0600

    commentschema created done

---
+++ src/models/Comment.ts
@@ -0,0 +1,19 @@
+import mongoose, { Schema, Document, Types } from 'mongoose';
+
+export interface IComment extends Document {
+  roadmapItemId: Types.ObjectId;
+  userId: Types.ObjectId;
+  content: string;
+  parentCommentId?: Types.ObjectId;
+  level: number;
+}
+
+const CommentSchema = new Schema<IComment>({
+  roadmapItemId: { type: Schema.Types.ObjectId, ref: 'RoadmapItem', required: true },
+  userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  content: { type: String, required: true, maxlength: 300 },
+  parentCommentId: { type: Schema.Types.ObjectId, ref: 'Comment', default: null },
+  level: { type: Number, default: 0 },
+}, { timestamps: true });
+
+export const Comment = mongoose.model<IComment>('Comment', CommentSchema);
