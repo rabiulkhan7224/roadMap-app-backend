32569498702fe0141d4fe707db4d7213d0fdab1a
rabiulkhan7224 <mdrabiulkhanbabo@gmail.com>
2025-06-16 22:48:28 +0600

    mongodb connection done

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,19 @@
 import express from 'express';
-
+import dotenv from 'dotenv';
+import { connectDB } from './config/db';
 const app= express()
 const PORT = process.env.PORT || 5000;
 
+dotenv.config();
+
+connectDB()
 
 app.get('/', (req, res) => {    
-    res.send('Hello World!')
+    res.send('roadmap app backend is running here')
+
     })
     app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`)
     })
+
 
