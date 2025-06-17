0a2e0af7a155425f467a756b6595f934a28b67c1
rabiulkhan7224 <mdrabiulkhanbabo@gmail.com>
2025-06-17 09:35:21 +0600

    login and logout funtionaliy done

--- src/controller/authController.ts
+++ src/controller/authController.ts
@@ -1,19 +1,45 @@
 import User from "../models/User"
 import { Request, Response } from "express";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-export const register = async (req:Request, res:Response) =>{
+export const register = async (req: Request, res: Response) => {
     const { name, email, password } = req.body
     try {
-        const existingUser= await User.findOne({email})
-        if(existingUser){
-             res.status(400).json({message:"User already exists"})
-             return;
+        const existingUser = await User.findOne({ email })
+        if (existingUser) {
+            res.status(400).json({ message: "User already exists" })
+            return;
         }
-const hashedPassword = await bcrypt.hash(password, 10)
-        const user= User.create({name, email, password: hashedPassword})
-        res.status(201).json({message: "registered successfully", user})
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const user = User.create({ name, email, password: hashedPassword })
+        res.status(201).json({ message: "registered successfully", user })
     } catch (error) {
         console.error("Error during registration:", error);
-        res.status(500).json({ message: "Internal server error , " });}
+        res.status(500).json({ message: "Internal server error , " });
+    }
+}
+export const login = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) return res.status(400).json({ message: 'Invalid credentials' })
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' })
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET!, { expiresIn: '7d' })
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'strict',
+        }).json({ message: 'Logged in' });
+    }
+    catch (error) {
+        console.error("Error during login:", error)
+        res.status(500).json({ message: "Internal server error" })
+    }
+
+}
+export const logout = (req: Request, res: Response) => {
+  res.clearCookie('token').json({ message: 'Logged out' });
 }
